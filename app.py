fdc72a27d3ef800d8d2c2288eabda402522cb3ef
sophi <94354648+sophii9@users.noreply.github.com>
2026-03-01 20:29:41 -0600

    Refactor ventas route to handle GET and POST methods //Corregido app.py

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
-from flask import Flask, render_template, request, jsonify, make_response, session
-
-from flask_cors import CORS, cross_origin
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/ventas')
-def ventas():
-    import mysql.connector
-    mydb = mysql.connector.connect(
+def get_connection():
+    return mysql.connector.connect(
         host="46.28.42.226",
         user="u760464709_24005366_usr",
         password="!|F>1$H1p",
         database="u760464709_24005366_bd"
     )
 
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM Ventas")
-    myresult = mycursor.fetchall()
-    return make_response(jsonify(myresult))
-
-@app.post('/ventas')
+@app.route('/ventas', methods=["GET", "POST"])
 def ventas():
-    import mysql.connector
-    mydb = mysql.connector.connect(
-        host="46.28.42.226",
-        user="u760464709_24005366_usr",
-        password="!|F>1$H1p",
-        database="u760464709_24005366_bd"
-    )
-
+    mydb = get_connection()
     mycursor = mydb.cursor()
-    sql = "INSERT INTO Ventas (ciudad, estado, codigo_postal, referencia) VALUES (%s, %s, %s, %s)"
-    val = (request.form['ciudad'], request.form['estado'], request.form['codigo_postal'], request.form['referencia'])
-    mycursor.execute(sql, val)
-    mydb.commit()
-    return "correcto"
+
+    if request.method == "GET":
+        mycursor.execute("SELECT * FROM Ventas")
+        myresult = mycursor.fetchall()
+        return make_response(jsonify(myresult))
+
+    elif request.method == "POST":
+        sql = "INSERT INTO Ventas (ciudad, estado, codigo_postal, referencia) VALUES (%s, %s, %s, %s)"
+        val = (request.form['ciudad'], request.form['estado'], request.form['codigo_postal'], request.form['referencia'])
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return make_response(jsonify({"estado": "ok"}))
