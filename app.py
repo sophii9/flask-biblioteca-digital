3a07a11bf93d482292aeb0cd2fec47f52cbd3694
sophi <94354648+sophii9@users.noreply.github.com>
2026-03-01 20:09:00 -0600

    Adaptación app.py a 4 campos de Ventas
    Added GET and POST routes for 'ventas' to handle database interactions.

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify, make_response, session
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/ventas')
+def ventas():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_24005366_usr",
+        password="!|F>1$H1p",
+        database="u760464709_24005366_bd"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM Ventas")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
+
+@app.post('/ventas')
+def ventas():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_24005366_usr",
+        password="!|F>1$H1p",
+        database="u760464709_24005366_bd"
+    )
+
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO Ventas (ciudad, estado, codigo_postal, referencia) VALUES (%s, %s, %s, %s)"
+    val = (request.form['ciudad'], request.form['estado'], request.form['codigo_postal'], request.form['referencia'])
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return "correcto"
